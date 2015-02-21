5020091c86783aabe5dc41291b3f724fe4c7a8c9
Manoj <scorp.sharma@gmail.com>
2015-02-21 16:09:10 +0000

    Commit Changes for Assignmnet 2
    Commit Changes for Assignmnet 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions use lexical scoping features 
+## for caching the inverse of an input matrix.
+## The inverse will be retuned from cache if the input matrix has not changed.
 
-## Write a short comment describing this function
+## This function list all the available functions for setting the matrix and inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        if(!identical(data,y)){
+            data <<- y
+            inverse <<- NULL
+        }
+    }
+    get <- function() data
+    setMatrix <- function(inv) inverse <<- inv
+    getMatrix <- function() inverse
+    list(set = set, get = get,
+         setMatrix = setMatrix,
+         getMatrix = getMatrix)
 }
 
 
-## Write a short comment describing this function
+## This function returns the matrix inverse using the caching features from makeCacheMatrix function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getMatrix()
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data, ...)
+    x$setMatrix(inverse)
+    return(inverse)   
 }
